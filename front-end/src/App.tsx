e88e5f43548204e30d2de8913022f6204cbbe284
BrenoBTrindade <brenokaminski@hotmail.com>
2023-04-17 11:45:17 -0300

    feat: configuration

---
+++ front-end/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Navigate to="/home" /> } />
+        <Route path="/home" element={ <Home /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ front-end/src/pages/home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <section>
+      Home
+    </section>
+  );
+}
+
+export default Home;
