ae5a8e3c688ce4d66326b0aa32580753532a6f38
BrenoBTrindade <brenokaminski@hotmail.com>
2023-04-17 19:10:41 -0300

    feat: routes for User

--- back-end/src/routes/users.routes.ts
+++ back-end/src/routes/users.routes.ts
@@ -3,6 +3,9 @@ import UserController from '../controller/users';
 
 const userRouter = Router();
 
+userRouter.post('/register', UserController.create);
 userRouter.get('/users', UserController.getALL);
+userRouter.get('/customer', UserController.getCustomer);
+userRouter.post('/login', UserController.employeeLogin);
 
 export default userRouter;
