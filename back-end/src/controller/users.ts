2ca520761c889ffe0e83e76c57edd0498245ddbc
BrenoBTrindade <brenokaminski@hotmail.com>
2023-04-17 19:10:23 -0300

    feat: methods for user Model

--- back-end/src/controller/users.ts
+++ back-end/src/controller/users.ts
@@ -2,8 +2,28 @@ import { Request, Response } from 'express';
 import UserService from '../services/users';
 
 export default class UserController {
+  static create = async (req: Request, res: Response) => {
+    const { username, role } = req.body;
+    const { code, message } = await UserService.create(username, role);
+    res.status(code).json({ message });
+  };
+
   static getALL = async (req: Request, res: Response) => {
     const { code, data } = await UserService.getAll();
     res.status(code).json({ data });
+  };
+
+  static getCustomer = async (req: Request, res: Response) => {
+    const { username } = req.body;
+    const { code, customer, message } = await UserService.getCustomer(username);
+    if (message) return res.status(code).json({ message });
+    res.status(code).json({ customer });
+  };
+
+  static employeeLogin = async (req: Request, res: Response) => {
+    const { username } = req.body;
+    const { code, message, id } = await UserService.employeeLogin(username);
+    if (message) return res.status(code).json({ message });
+    res.status(code).json({ employeeId: id });
   };
 }
--- back-end/src/services/users.ts
+++ back-end/src/services/users.ts
@@ -1,8 +1,28 @@
 import Users from '../database/models/users';
 
 export default class UserService {
+  static create = async (username: string, role: string) => {
+    if (!username || !role) return { code: 400, message: 'missing name or role' };
+    const user = await Users.findOne({ where: { username, role: 'customer' } });
+    if (user) return { code: 401, message: 'User already exists' };
+    await Users.create({ username, role });
+    return { code: 201, message: 'created' };
+  };
+
   static getAll = async () => {
     const allUsers = await Users.findAll();
     return { code: 200, data: allUsers };
+  };
+
+  static getCustomer = async (username: string) => {
+    const customer = await Users.findOne({ where: { username, role: 'customer' } });
+    if (!customer) return { code: 401, message: 'not Found' };
+    return { code: 200, customer };
+  };
+
+  static employeeLogin = async (username: string) => {
+    const employee = await Users.findOne({ where: { username, role: 'employee' } });
+    if (!employee) return { code: 401, message: 'not found' };
+    return { code: 200, id: employee.id };
   };
 }
