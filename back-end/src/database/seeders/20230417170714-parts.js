c66375c8b429c4718d8a032bd94f5d3f9304adfd
BrenoBTrindade <brenokaminski@hotmail.com>
2023-04-18 15:06:28 -0300

    feat: parts in sedeers

--- back-end/src/database/seeders/20230417170714-parts.js
+++ back-end/src/database/seeders/20230417170714-parts.js
@@ -11,6 +11,14 @@ module.exports = {
         name: 'radiador',
         price: 400
       },
+      {
+        name: 'correia',
+        price: 150
+      },
+      {
+        name: 'suspencao',
+        price: 2000
+      },
     ], {});
   },
 
